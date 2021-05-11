a5bf577fe961f73d319bb5b5dd3da1253432f262
Robin Scherrer <43877944+rxbn@users.noreply.github.com>
2021-05-11 12:52:49 +0200

    bump syncflaer to v4.0.1

--- Formula/syncflaer.rb
+++ Formula/syncflaer.rb
@@ -1,24 +1,24 @@
 class Syncflaer < Formula
     desc "SyncFlaer"
     homepage "https://containeroo.ch"
-    version "4.0.0"
+    version "4.0.1"
     bottle :unneeded
 
     if OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.0/syncflaer-v4.0.0-darwin-amd64.tar.gz"
-      sha256 "d42c9b4dbc2954d9bbfd46a1206d586471c54c12e2cdfbd92d626dc508850d66"
+      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.1/syncflaer-v4.0.1-darwin-amd64.tar.gz"
+      sha256 "fee7cafb1354a333dbba8d1d97558669a6d419e1e4836316a3d0047cddcdc700"
     end
     if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.0/syncflaer-v4.0.0-darwin-arm64.tar.gz"
-      sha256 "359c2eca1083a3050c59b26fe46f5a6a7d898982023e27443bee9041b4fcbd34"
+      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.1/syncflaer-v4.0.1-darwin-arm64.tar.gz"
+      sha256 "ce99d29b8d6b68aa899cceec98fee6d421229f57bca10b3ea5c78541793c7331"
     end
     if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.0/syncflaer-v4.0.0-linux-amd64.tar.gz"
-      sha256 "0b366a52e5a12c26d2e692b6f639b273280d3f405f5ceb7f016b17b9ef1e51ed"
+      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.1/syncflaer-v4.0.1-linux-amd64.tar.gz"
+      sha256 "611cbd8bd26e1551cebda447934f0270dd2081af14709f647a74574587f7ba9a"
     end
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.0/syncflaer-v4.0.0-linux-arm64.tar.gz"
-      sha256 "51df6d00e2fb2bbbab49134aa400957e3dfa5025ae7ee6e1a786d3a4a2e44b6b"
+      url "https://github.com/containeroo/syncflaer/releases/download/v4.0.1/syncflaer-v4.0.1-linux-arm64.tar.gz"
+      sha256 "18135d9d37f53d12454ddb175fb5ea34fb6f4cdc5c8d809b60c893b6835f19bf"
     end
 
     def install
