d393ae1f8c694e9b1ba7152853ba2e420293c541
gi8 <giottolino@gmail.com>
2021-08-10 15:44:18 +0200

    certinfo: correct sha256

--- Formula/certinfo.rb
+++ Formula/certinfo.rb
@@ -6,19 +6,19 @@
 
     if OS.mac? && Hardware::CPU.intel?
       url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-darwin-amd64.tar.gz"
-      sha256 "10fe5dbb3782ee2d81e736d69fcc8697bfe1d2682ddd86dc9ab740ab2123773f"
+      sha256 "bea9835f367de68fb490dc30cf6acb65fa42f3603b3dbe55e19c075ba1e486f8"
     end
     if OS.mac? && Hardware::CPU.arm?
       url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-darwin-arm64.tar.gz"
-      sha256 "98180fc908fc906d3c081d31f581127d2fd560fde60f4bb050306d350fe5b002"
+      sha256 "33e5671b4f58a4b321e0d8a6d950eac4bd1ae534183722cc74d89c96dd6c7efe"
     end
     if OS.linux? && Hardware::CPU.intel?
       url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-linux-amd64.tar.gz"
-      sha256 "bb4b361b047fbfc08fed1ff05c5ec7e846a6ad681bbb0b7265a9ad353d25ec60"
+      sha256 "5f7b7fd3f1bf35a99f1885e8942d942b2791f8275eeb78cd78cdb0ddee02f392"
     end
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-linux-arm64.tar.gz"
-      sha256 "14c9749cedd639b535f00cc7c103817c9cecc7c1c5119531a0a45767e6e76df0"
+      sha256 "d6e3bb82222823adcd5d68415a7f690f8a9eb15f89fdfc525a9c2e397533640c"
     end
 
     def install
