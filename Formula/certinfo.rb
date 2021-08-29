ae0b3b60dd72be3cc57f8518b77bdb7ccf7230a2
gi8 <giottolino@gmail.com>
2021-08-29 18:55:55 +0200

    chore(certinfo): update to v1.2.0

--- Formula/certinfo.rb
+++ Formula/certinfo.rb
@@ -1,24 +1,24 @@
   class Certinfo < Formula
     desc "certinfo"
     homepage "https://containeroo.ch"
-    version "1.1.0"
+    version "1.2.0"
     bottle :unneeded
 
     if OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-darwin-amd64.tar.gz"
-      sha256 "bea9835f367de68fb490dc30cf6acb65fa42f3603b3dbe55e19c075ba1e486f8"
+      url "https://github.com/containeroo/certinfo/releases/download/v1.2.0/certinfo-v1.2.0-darwin-amd64.tar.gz"
+      sha256 "581364f8acdbbc00c82b1ded10b080e2e676131398658bb1c4b100d029bc4292"
     end
     if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-darwin-arm64.tar.gz"
-      sha256 "33e5671b4f58a4b321e0d8a6d950eac4bd1ae534183722cc74d89c96dd6c7efe"
+      url "https://github.com/containeroo/certinfo/releases/download/v1.2.0/certinfo-v1.2.0-darwin-arm64.tar.gz"
+      sha256 "9f1ac5fe8f80e38af011947ed9de6b4b20805667a8669029828094531e94439e"
     end
     if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-linux-amd64.tar.gz"
-      sha256 "5f7b7fd3f1bf35a99f1885e8942d942b2791f8275eeb78cd78cdb0ddee02f392"
+      url "https://github.com/containeroo/certinfo/releases/download/v1.2.0/certinfo-v1.2.0-linux-amd64.tar.gz"
+      sha256 "eda686638d519942061ccf043ad1026dbae897bc2bb19b11eb1b9660fe42109f"
     end
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/containeroo/certinfo/releases/download/v1.1.0/certinfo-v1.1.0-linux-arm64.tar.gz"
-      sha256 "d6e3bb82222823adcd5d68415a7f690f8a9eb15f89fdfc525a9c2e397533640c"
+      url "https://github.com/containeroo/certinfo/releases/download/v1.2.0/certinfo-v1.2.0-linux-arm64.tar.gz"
+      sha256 "e6e3ab8cc8c3ed91fcfe131360adb11b56a4a150b81c2a6fcf11412a7f54095e"
     end
 
     def install
